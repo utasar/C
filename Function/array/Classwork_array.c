de330c3f631f467ce5baba648934d7f466953592
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:21:18 -0500

    Create Classwork_array.c

---
+++ Function/array/Classwork_array.c
@@ -0,0 +1,15 @@
+//2. a[]={10,20,30,40,50}Print using for loop#include<stdio.h>
+
+#include<stdio.h>
+int main()
+{
+    int a[]={10,20,30,40,50};
+
+    printf("displaying integers:");
+    //printing elements of an array
+    for(int i=0;i<5;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
