7b0bf8f8953e13269d944c4cf22e4ad29682f635
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:12:20 -0500

    Create unary1.c

---
+++ Example/unary1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n=1;
+    printf("first step n: value of n is %d\n",n);
+     printf("second step n++: value of n is %d\n",n++);
+      printf("third step n: value of n is %d\n",n);
+       printf("second step n--: value of n is %d\n",n--);
+        printf("third step n: value of n is %d\n",n);
+        return 0;
+}
