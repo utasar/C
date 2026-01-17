02d50d9b4ac79a1a1094379806f2ecd96058f298
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:58:19 -0500

    Create int.c

---
+++ Example/int.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    //Integer Integer type
+    int a=5/2;
+    printf("value of 5/2 is%d\n",a);
+
+    //float and integer
+    float b=5.0/2;
+     printf("value of 5.o/2 is %d\n",b);
+    return 0;
+}
