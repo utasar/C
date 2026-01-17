7382a1e2763c5bf3c75296e117ef77247984dcbd
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:00:44 -0500

    Create modulus.c

---
+++ Example/modulus.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+ int a=5%2;
+    printf("value of a is %d\n",a);
+
+    int b=2%5;
+      printf("value of b is %d\n",b);
+
+      int c= -5%2;
+        printf("value of c is %d\n",c);
+
+        int d= 5%-2;
+          printf("value of d is %d\n",d);
+
+          int e= -5%-2;
+            printf("value of e is %d\n",e);
+
+          int f= 5.3%2;
+            printf("value of f is %d\n",f);
+            return 0;
+}
