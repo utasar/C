6155964a7441890e5dbf74549a875eebc744c578
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:41:03 -0500

    Create addition.c

---
+++ Function/addition.c
@@ -0,0 +1,16 @@
+// program to find the arthmetic operations of two numbers
+#include<stdio.h>
+int main()
+{
+  int a=50;
+  int b=20;
+  int sum=a+b;
+  printf("addition of a and b is %d\n",sum); 
+  int diff=a-b;
+printf("difference of a and b is %d\n",diff);
+int multiply=a*b;
+ printf("multiplication of a and b is %d\n",multiply); 
+ int remainder=a/b;
+  printf("remainder of a and b is %d\n",remainder); 
+    return 0;
+}
