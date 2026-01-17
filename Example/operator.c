47ed90a430d49d9f4d3df752f8d64f5251a5e0ec
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:04:18 -0500

    Create operator.c

---
+++ Example/operator.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+    // main function
+    int main()
+{
+//assignment operator(=)
+int a,b;
+a=10;
+printf("value of a is %d\n",a);
+     //adding value to a
+     a=a+5;
+     printf("value of updated a is %d\n",a);
+
+     //initializing (assigning) value of a to b
+     b=a;
+     printf("value of b is %d\n",b);
+
+     //assigment is from right to left
+     int p,q;
+
+     
+     p=q=40;
+     printf("value of p is %d\n",p);
+     printf("value of q is %d\n",q);
+return 0;
+}
