830edd22b45c6a856048d8ac7f930bc5ec91e06c
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:10:10 -0500

    Create task.c

---
+++ Example/task.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    a=10;
+    printf("In 1st step:The value of a is %d\n",a);
+    a=15;
+    printf("In 2nd step:The value of a is %d\n",a);
+    int b=a;
+    printf("In 3rd step:The value of b is %d\n",b);
+    return 0;
+}
