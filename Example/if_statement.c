eee649f2bd90dbc94ad43d610069a13c5b36c62f
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:57:28 -0500

    Create if_statement.c

---
+++ Example/if_statement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //declare variable
+    int age;
+// initialize variable
+age=19;
+//check the condition
+int result=(age>=18);
+printf("The condition for if is %d\n ",result);
+   //check if the person can vote
+   if (age>=18)
+   {
+    //display if the condition is satisfied
+    printf("You are elegible to vote!\n");
+   }
+  // printf("code runs here\n");
+   return 0;
+}
