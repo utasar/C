450602a64398173ebf55d664191c999667bf8fcc
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:43:44 -0500

    Create armstrong.c

---
+++ Example/armstrong.c
@@ -0,0 +1,9 @@
+//Write different programs to find out if the given numvber(entered by the user) is armstrong number or not using for loop 
+
+#include<stdio.h>
+int main()
+{
+  int num; printf("Enter a number:");
+  scanf("%d", &num);
+  for (int i=1;)
+}
