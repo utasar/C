a26a00e6ef8ef6cabd829738b775519255def958
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:53:53 -0500

    Create float.c

---
+++ Example/float.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+      //float and float 
+    float a=5/2;
+    printf("value of 5/2 is %f\n",a);
+
+    float b= 5.0/2;
+    printf("value of 5.0/2 is %f\n",b);
+
+    float c=2/5;
+     printf("value of 2/5 is %f\n",c);
+
+     float d=2.0/5.0;
+     
+      printf("value of 2.0/5.0 is %f\n",d);
+        return 0;
+}
