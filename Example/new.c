79d714a4a4e32c787c58a86b994c1a2df8385d82
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:02:06 -0500

    Create new.c

---
+++ Example/new.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=3.14;
+    int b=a;
+    printf("The value of a is %d\n",b);
+
+    //float f=10.5;
+    //int i=f;
+
+    //printf("the value of f is %d\n",f);
+    //printf("the value of i is %d\n ",i);
+    return 0;
+}
