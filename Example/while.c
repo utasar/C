1b832912faa9ec0778937a3d3c34cfbe9825978c
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 23:12:53 -0500

    Create while.c

---
+++ Example/while.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+    // declare and initialize variable
+    int i = 0;
+
+    //while loop 
+    /* while (condition) // while(1) -. enters inside loop 
+    {
+        printf("%d\n",i); //print the value of i 
+        i=i+1;            //increase the value of i(counter)
+    }
+    */
+   while (i<5){
+    printf("%d\n",i);
+    i=i+1; 
+   }
+   return 0;
+}
+
