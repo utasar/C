5dad07fe633ad05c9e36dc613cfcc0ecc1fdab0e
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:38:43 -0500

    Create string.c

---
+++ Hello/string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    // Declaring a string
+    // This array can hold up to 19 characters plus the null terminator
+    char myString[20];
+    // Initializing a string
+    char anotherString[10] = "Hello";
+
+    //Printing a string
+
+    printf("anotherString: %s\n", anotherString);
+
+    return 0;
+}
