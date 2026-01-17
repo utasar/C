22e946da9ffb4c9b9e9fc501577f0441889ba936
Utsav Acharya <104833358+utasar@users.noreply.github.com>
2026-01-16 22:29:25 -0500

    Create function_type.c

---
+++ Function/function_type.c
@@ -0,0 +1,33 @@
+//Return type(int,float,char)
+//No return type (void)
+//Arguments function_name(type 1 arg 1,type2 arg2)
+//No arguments function_name()
+#include<stdio.h>
+
+/*
+1.Function declaration
+syntax:
+return type function_name(type 1 argument 1,....type n argument n);
+
+*/
+
+int main()
+{
+    /*
+    3.Function call
+    syntax:
+    function_name(argument 1,.....argument n);
+    */
+
+
+   return 0;
+}
+
+/*
+2.Function declaration 
+syntax:
+return type function_ name(type 1 argument 1,....type n argument n)
+{
+    function_body;
+}
+*/
